504724e5dfce3c19402c9fdaebb9f1bf54169e0e
YuliiaIIIII <yulyamashka@gmail.com>
2024-01-05 23:56:24 +0100

    Added functional button to user_info.html

--- aboutUser.js
+++ aboutUser.js
@@ -2,9 +2,9 @@ const userDetails = document.querySelector(".user_details");
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
 function getUserId () {
-    const urlUserId = window.location.search;
-    const urlParam = new URLSearchParams(urlUserId);
-    return urlParam.get('user_id');
+	const urlUserId = window.location.search;
+	const urlParam = new URLSearchParams(urlUserId);
+	return urlParam.get('user_id');
 }
 
 const userId = getUserId();
@@ -17,29 +17,27 @@ fetch(`${USERS_URL}/${userId}`)
 
 		Object.keys(user).forEach(key => {
 		valuesArray.push(user[key]);
-
-
     });
 	
 	function addInfo() {
 		const infoItems = [
-			{ info_title: "Ім'я", info_value: user.name },
+			{ info_title: "Ім'я користувача", info_value: user.name },
 			{ info_title: "Нікнейм", info_value: user.username },
 			{ info_title: "E-mail", info_value: user.email },
 			{ info_title: "Контакти", info_value: user.phone },
 			{ info_title: "Кампанія", info_value: user.company.name },
 		];
-	
+
 		infoItems.forEach(element => {
+			const div = document.createElement('div');
+			div.setAttribute('class', 'info_block');
+			userDetails.appendChild(div);
 			const infoElement = document.createElement('p');
-			infoElement.classList.add('info_title');
-			userDetails.appendChild(infoElement);
-			infoElement.innerHTML = `<h4 class="user_title">${element.info_title}:</h4><p>${element.info_value}</p>`;
+			div.appendChild(infoElement);
+			div.innerHTML = `<h4 class="user_title">${element.info_title}:</h4><p>${element.info_value}</p>`;
 		});
 	}
 	
 	addInfo();
-
-    console.log("Масив значень:", valuesArray); 
 })
 .catch(error => console.error("Помилка отримання даних", error));
--- script.js
+++ script.js
@@ -9,41 +9,41 @@ const ALL_USERS = 'users';
 const ALL_POSTS = 'posts';
 
 fetch(API_URL + ALL_POSTS)
-    .then(response => response.json())
-    .then(posts => {
-        posts = posts.sort(() => Math.random() - 0.5).slice(0, 10);
-        posts.forEach(post => {
-        const postElement = document.createElement("div");
-        postElement.innerHTML = `<h4>${post.title}</h4><p>${post.body}</p>`;
-        userPosts.appendChild(postElement);
-    });
+	.then(response => response.json())
+	.then(posts => {
+		posts = posts.sort(() => Math.random() - 0.5).slice(0, 10);
+		posts.forEach(post => {
+		const postElement = document.createElement("div");
+		postElement.innerHTML = `<h4>${post.title}</h4><p>${post.body}</p>`;
+		userPosts.appendChild(postElement);
+	});
 });
 
 async function getUserList() {
-    
-    const allUsers = await fetch(API_URL + ALL_USERS);
-    const users = await allUsers.json();
+	
+	const allUsers = await fetch(API_URL + ALL_USERS);
+	const users = await allUsers.json();
 
-    users.forEach(user => {
-        function addUser() {
-            const userCard = document.createElement('div');
-            userCard.setAttribute('class', 'userCard');
-            userList.appendChild(userCard);
+	users.forEach(user => {
+		function addUser() {
+			const userCard = document.createElement('div');
+			userCard.setAttribute('class', 'userCard');
+			userList.appendChild(userCard);
 
-            let imgDiv = document.createElement('div');
-            imgDiv.setAttribute('class', 'userAvatar');
-            userCard.appendChild(imgDiv);
+			let imgDiv = document.createElement('div');
+			imgDiv.setAttribute('class', 'userAvatar');
+			userCard.appendChild(imgDiv);
 
-            let nameDiv = document.createElement('div');
-            nameDiv.setAttribute('class', 'userName');
-            userCard.appendChild(nameDiv);
+			let nameDiv = document.createElement('div');
+			nameDiv.setAttribute('class', 'userName');
+			userCard.appendChild(nameDiv);
 
-            let nameName = document.createElement('a');
-            nameName.textContent = user.name;
-            nameName.setAttribute('href', `user_info.html?user_id=${user.id}`);
-            nameDiv.appendChild(nameName);
-        }
-        addUser(user)
-    });  
+			let nameName = document.createElement('a');
+			nameName.textContent = user.name;
+			nameName.setAttribute('href', `user_info.html?user_id=${user.id}`);
+			nameDiv.appendChild(nameName);
+		}
+		addUser(user)
+	});  
 }
 getUserList ()
